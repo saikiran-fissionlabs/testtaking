fca6b2a863f421144d0c666066d1ef0cc64686dd
saikiran-fissionlabs <99412660+saikiran-fissionlabs@users.noreply.github.com>
2022-07-21 19:18:13 +0530

    styles

--- src/components/home/Carousel.js
+++ src/components/home/Carousel.js
@@ -1,28 +1,14 @@
 import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-// import MobileStepper from '@mui/material/MobileStepper';
-// import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-// import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-// import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-// import SwipeableViews from 'react-swipeable-views';
-// import { autoPlay } from 'react-swipeable-views-utils';
 
-import testNow from '../../assets/images/testNow.png'
-// const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
-// const bull = (
-//   <Box
-//     component="span"
-//     sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-//   >
-//     •
-//   </Box>
-// );
+import testNow from '../../assets/images/testNow.png';
+
 const cards = [
   {
     label: 'Test -1',
@@ -50,62 +36,23 @@ const cards = [
 function SwipeableTextMobileStepper() {
   const theme = useTheme();
   const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = cards.length;
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
 
   const handleStepChange = (step) => {
     setActiveStep(step);
   };
 
   return (
-    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
-      {/* <Paper
-        square
-        elevation={0}
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          height: 50,
-          pl: 2,
-          bgcolor: 'background.default',
-        }}
-      >
-        <Typography>{images[activeStep].label}</Typography>
-      </Paper> */}
+    <Box sx={{ maxWidth: 400, flexGrow: 2 }}>
       <div
         axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
         index={activeStep}
         onChangeIndex={handleStepChange}
         enableMouseEvents
+        className='cards'
       >
-        {/* {images.map((step, index) => (
-          <div key={step.label}>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <Box
-                component="img"
-                sx={{
-                  height: 255,
-                  display: 'block',
-                  maxWidth: 400,
-                  overflow: 'hidden',
-                  width: '100%',
-                }}
-                src={step.imgPath}
-                alt={step.label}
-              />
-            ) : null}
-          </div>
-        ))} */}
         {cards.map((step, index) =>
-          <Card sx={{ minWidth: 275 }}>
-            <CardContent>
+          <Card sx={{ minWidth: 275, margin: '10px' }} key={index} className='card' >
+            <CardContent className='cardContent'>
               <Typography variant="h4" color="text.secondary" gutterBottom>
                 {step.label}
               </Typography>
@@ -120,7 +67,6 @@ function SwipeableTextMobileStepper() {
               </Typography>
             </CardContent>
             <CardActions>
-              {/* <Button size="small">TEST NOW</Button> */}
               <Box sx={{ width: '100%' }}>
                 <img src={testNow} className="App-logo" alt='logo' />
               </Box>
@@ -128,36 +74,6 @@ function SwipeableTextMobileStepper() {
           </Card>
         )}
         </div>
-      {/* </AutoPlaySwipeableViews> */}
-      {/* <MobileStepper
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      /> */}
     </Box>
   );
 }
